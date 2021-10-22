9c64b8959497d05448385e0901ea817f79476cd9
Камиль Нагаев <shumnger@mail.ru>
2021-10-22 14:37:44 +0600

    решение

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -7,7 +7,6 @@ public class Island2 {
     public Island2(Wood3 wood) {
         this.wood = wood;
     }
-
     @Override
     public String toString() {
         return ", на острове дерево " + wood.toString();
